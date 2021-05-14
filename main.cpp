be249dca592a0e763185b05c3ce91a37c4364a36
WYATTWANGKUI <83682792+WYATTWANGKUI@users.noreply.github.com>
2021-05-14 23:04:57 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <windows.h>
+//This experiment is done on WINXP OS. 
+//using WINDBG command below to modify WINXP's LDT and GDT, to create a interrupt gate0000E500`00480000 at the address 0x8003f400(inside LDT) 
+//and a new TSS descriptor 0000E912`FF040068 at the address 0x8003f048(inside GDT).The size of a TSS table is 0x68 bytes.
+//eq 8003f400 0000E500`00480000  eq 8003f048 0000E912`FF040068(The base address of the new TSS Table is at 0x0012FF04 in this case)
+//WINXP is using its default TSS descriptor 0x80008b04`0x200020ab at address 0x8003f028, where 0x80042000 is the base of the default TSS table that WINXP is using.
 
 
 DWORD dwGDT48;
@@ -7,30 +12,26 @@ DWORD dwEFLAG;
 DWORD dwTSS_EFLAG;
 
 void __declspec(naked) func() {
-
-
-
-	__asm {
-// 		pushad
+  __asm {
+            pushad
             pushfd
-			mov eax, dword ptr ds:[esp]
-			mov dwEFLAG, eax
-			add esp, 0x00000004
+		    
+//the below instructions serve to get the current EFLAG value and put it into dwEFLAG  
+  mov eax, dword ptr ds:[esp]
+  mov dwEFLAG, eax
+  
+//the below instructions serve to get the EFLAG value being stored in the new TSS table, and put this EFLAG value into dwTSS_EFLAG	  
+  mov eax, dword ptr ds:[0x0012FF04+0x24] //(the base address of the new TSS Table is at 0x0012FF04. EFLAG is stored at the offset 0x24 ) 
+  mov dwTSS_EFLAG, eax
 
-			mov eax, dword ptr ds:[0x0012FF04+0x24] // (TSS Table @ 0012FF04)
-			mov dwTSS_EFLAG, eax
+//WINXP is using its default TSS descriptor 0x80008b04`0x200020ab at address 0x8003f028, where 0x80042000 is the base of the default TSS table that WINXP is using.
+//The below instructions serve to ensure the old default TSS descriptor is in correct form so that it will work properly when using iret at the end.
+mov dword ptr ds:[0x8003f028], 0x200020ab
+mov dword ptr ds:[0x8003f02c], 0x80008b04 //when using jmp instead of int/call to switch to a new TSS descriptor, need to change 9 to b which means from available(9) to busy(b)
 
-		    mov dword ptr ds:[0x8003f02c], 0x80008b04 // when using jmp, change 9 to b
-            mov dword ptr ds:[0x8003f028], 0x200020ab
-
-			mov eax, dword ptr ds:[0x8003f04c] 
-			mov dwGDT48, eax
-
-			
-			
-// 		popfd
-// 	    popad
-			iret
+            popfd
+ 	    popad
+iret
 	}
 }
 
@@ -39,11 +40,11 @@ void __declspec(naked) func() {
 int main(int argc, char* argv[])
 {
 
-	char bu[0x10]={0};
+	char esp_buff[0x10]={0};
 	int iCr3=0;
 
 	DWORD tss[26] = {
-		0x00000000,//link£¬ÓÉcpu×Ô¶¯Ìî³ä
+		0x00000000,//link
 			0x00000000,//esp0
 			0x00000000,//ss0
 			0x00000000,//esp1
@@ -57,7 +58,7 @@ int main(int argc, char* argv[])
 			0x00000000,//ecx
 			0x00000000,//edx
 			0x00000000,//ebx
-			(DWORD)bu,//esp
+			(DWORD)esp_buff,//esp
 			0x00000000,//ebp
 			0x00000000,//esi
 			0x00000000,//edi
@@ -70,25 +71,23 @@ int main(int argc, char* argv[])
 			0x00000000,//ldtr
 			0x20ac0000
 	};
-	printf("tss=%p esp=%p func=%p", tss, bu,func);
+	printf("tss=%p esp=%p func=%p", tss, esp_buff, func);
 	printf("please input iCr3:\n");
 	scanf("%x", tss+7);
 	
-	char buff[6];
-	*(DWORD*)&buff[0] = 0x12345678;
-	*(WORD*)&buff[4] = 0x48;
+	char buffer[6];
+	*(DWORD*)&buffer[0] = 0x12345678;
+	*(WORD*)&buffer[4] = 0x48;
 	
 	_asm {
-   		str tss
+   		str tss 
 			
-		int 0 //eq 8003f400 0000E500`00480000  eq 8003f048 0000E912`FF040068(TSS Table @ 0012FF04)
-		//call fword ptr ds:[buff]
-		//jmp 0x0048:0x12345678
+		int 0 
 	}
-//  	int * p =(int *)0x8003f048; //0x004011E5,//eip
-//  	int n =*p;
-    int tss_eflg =(int) *(tss +9) ;
-	printf("\n TSS.EFLG AFTER CALL=%p\n,GDT48 AFTER JMP=%p\n, EFLG BEFORE IRET=%p, EFLG IN TSS AFTER IRET=%p", dwTSS_EFLAG, dwGDT48, dwEFLAG, tss_eflg);
+
+	
+        int TSS_EFLAG =(int) *(tss +9) ;
+	printf("\n TSS.EFLG AFTER CALL=%p\n, EFLG BEFORE IRET=%p, EFLG IN TSS AFTER IRET=%p", dwTSS_EFLAG, dwEFLAG, TSS_EFLAG);
 	getchar();getchar();
 	return 0;
 }
